0b74ed253d7d4070d1bda1cddb1c524ff8e3f2b4
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-04-26 09:44:35 -0500

    test for file clean up

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,8 @@
+FROM golang:1.18 AS builder
+WORKDIR /build
+COPY . .
+RUN go build -ldflags="-s -w" -o sage-storage-loader
 
-
-# docker build -t waggle/sage-uploader .
-# docker run -ti  waggle/sage-uploader
-# for development:
-# docker run -ti --rm --env-file ./.env -v ${PWD}:/go/src/app/ -v ${PWD}/temp/rmq/:/tls/ -v ${PWD}/temp/ca/:/ca/ --entrypoint /bin/bash waggle/sage-uploader
-
-FROM golang:1.16
-
-
-RUN apt-get update && apt-get install -y inotify-tools
-
-WORKDIR /go/src/app
-
-
-COPY *.go ./go.*  /go/src/app/
-ADD rwmutex /go/src/app/rwmutex
-
-# create go.mod
-#> go mod init github.com/sagecontinuum/sage-storage-loader
-# update go.mod
-#> go mod tidy
-#RUN go get -u gotest.tools/gotestsum
-
-
-# got get ... will create/append go.sum
-
-RUN go build -o uploader
-
-
-ENTRYPOINT [ "./uploader" ]
-CMD []
+FROM scratch
+COPY --from=builder /build/sage-storage-loader /sage-storage-loader
+ENTRYPOINT [ "/sage-storage-loader" ]
--- worker_test.go
+++ worker_test.go
@@ -6,17 +6,61 @@ import (
 	"testing"
 )
 
+type testfile struct {
+	path   string
+	data   []byte
+	upload string
+}
+
+func tempDirFromTestFiles(t *testing.T, testfiles []testfile) string {
+	// generate temp directory and write out all test files
+	root := filepath.Join(t.TempDir(), "data")
+
+	for _, file := range testfiles {
+		path := filepath.Join(root, file.path)
+
+		// ensure parents exist
+		if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+			t.Fatal(err)
+		}
+
+		// write file
+		if err := os.WriteFile(path, file.data, 0o644); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return root
+}
+
+type pair struct{ src, dst string }
+
+type mockUploader struct {
+	Error   error
+	Uploads map[pair]bool
+}
+
+func newMockUploader() *mockUploader {
+	return &mockUploader{
+		Uploads: make(map[pair]bool),
+	}
+}
+
+func (up *mockUploader) UploadFile(src, dst string, meta *MetaData) error {
+	if up.Error != nil {
+		return up.Error
+	}
+	up.Uploads[pair{src, dst}] = true
+	return nil
+}
+
 func TestScanAndProcess(t *testing.T) {
-	testcases := map[string][]struct {
-		path   string
-		data   []byte
-		upload string
-	}{
+	testcases := map[string][]testfile{
 		"NoJob": {
 			{
 				path:   "node-000048b02d15bc7c/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
 				upload: "node-data/sage/sage-imagesampler-top-0.2.5/000048b02d15bc7c/1638576647406523064-wow1.txt",
-				data:   []byte(``),
+				data:   []byte(`some data`),
 			},
 			{
 				path:   "node-000048b02d15bc7c/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -28,7 +72,7 @@ func TestScanAndProcess(t *testing.T) {
 			{
 				path:   "node-000048b02d15bc7c/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
 				upload: "node-data/Pluginctl/sage-imagesampler-top-0.2.5/000048b02d15bc7c/1638576647406523064-wow1.txt",
-				data:   []byte(``),
+				data:   []byte(`some more data`),
 			},
 			{
 				path:   "node-000048b02d15bc7c/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -45,13 +89,13 @@ func TestScanAndProcess(t *testing.T) {
 		"SkipNoMeta": {
 			{
 				path: "node-000048b02d15bc7c/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
-				data: []byte(``),
+				data: []byte(`where's my meta file?`),
 			},
 		},
 		"SkipDone": {
 			{
 				path: "node-000048b02d15bc7c/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
-				data: []byte(``),
+				data: []byte(`we already uploaded this`),
 			},
 			{
 				path: "node-000048b02d15bc7c/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -66,7 +110,7 @@ func TestScanAndProcess(t *testing.T) {
 			{
 				path:   "node-0000000000000001/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
 				upload: "node-data/sage/sage-imagesampler-top-0.2.5/0000000000000001/1638576647406523064-wow1.txt",
-				data:   []byte(``),
+				data:   []byte(`all of these`),
 			},
 			{
 				path:   "node-0000000000000001/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -76,7 +120,7 @@ func TestScanAndProcess(t *testing.T) {
 			{
 				path:   "node-0000000000000002/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
 				upload: "node-data/sage/sage-imagesampler-top-0.2.5/0000000000000002/1638576647406523064-wow2.jpg",
-				data:   []byte(``),
+				data:   []byte(`will be`),
 			},
 			{
 				path:   "node-0000000000000002/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -86,7 +130,7 @@ func TestScanAndProcess(t *testing.T) {
 			{
 				path:   "node-0000000000000003/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
 				upload: "node-data/Pluginctl/sage-imagesampler-top-0.2.5/0000000000000003/1638576647406523064-wow3.flac",
-				data:   []byte(``),
+				data:   []byte(`uploaded together`),
 			},
 			{
 				path:   "node-0000000000000003/uploads/Pluginctl/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -94,20 +138,20 @@ func TestScanAndProcess(t *testing.T) {
 				data:   []byte(`{"ts":1638576647406523064,"labels":{"filename":"wow3.flac"}}`),
 			},
 		},
-		"MissingMeta": {
+		"MissingMetaField": {
 			{
 				path: "node-000048b02d15bc7c/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
-				data: []byte(``),
+				data: []byte(`we don't know this filename`),
 			},
 			{
 				path: "node-000048b02d15bc7c/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
 				data: []byte(`{"ts":1638576647406523064}`),
 			},
 		},
-		"MissingMetaFilename": {
+		"MissingMetaFilenameField": {
 			{
 				path: "node-000048b02d15bc7c/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/data",
-				data: []byte(``),
+				data: []byte(`we still don't know this filename`),
 			},
 			{
 				path: "node-000048b02d15bc7c/uploads/imagesampler-top/0.2.5/1638576647406523064-9801739daae44ec5293d4e1f53d3f4d2d426d91c/meta",
@@ -116,20 +160,9 @@ func TestScanAndProcess(t *testing.T) {
 		},
 	}
 
-	for name, files := range testcases {
+	for name, testfiles := range testcases {
 		t.Run(name, func(t *testing.T) {
-			// generate temp directory and write out all test files
-			root := filepath.Join(t.TempDir(), "data")
-
-			for _, file := range files {
-				path := filepath.Join(root, file.path)
-				if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
-					t.Fatal(err)
-				}
-				if err := os.WriteFile(path, file.data, 0o644); err != nil {
-					t.Fatal(err)
-				}
-			}
+			root := tempDirFromTestFiles(t, testfiles)
 
 			// scan for jobs and feed to worker
 			stop := make(chan struct{})
@@ -141,8 +174,11 @@ func TestScanAndProcess(t *testing.T) {
 				close(jobs)
 			}()
 
-			uploader := NewMockUploader()
-			worker := &Worker{Uploader: uploader}
+			uploader := newMockUploader()
+			worker := &Worker{
+				DeleteFilesOnSuccess: true,
+				Uploader:             uploader,
+			}
 
 			for job := range jobs {
 				worker.Process(job)
@@ -151,7 +187,7 @@ func TestScanAndProcess(t *testing.T) {
 			// check that all expected uploads exist
 			uploads := make(map[pair]bool)
 
-			for _, file := range files {
+			for _, file := range testfiles {
 				if file.upload != "" {
 					uploads[pair{filepath.Join(root, file.path), file.upload}] = true
 				}
@@ -168,27 +204,14 @@ func TestScanAndProcess(t *testing.T) {
 					t.Fatalf("unexpected upload\nsrc: %s\ndst: %s", k.src, k.dst)
 				}
 			}
+
+			for k := range uploads {
+				if uploader.Uploads[k] {
+					if _, err := os.Stat(k.src); err == nil {
+						t.Fatalf("file should have been cleaned up: %s", k.src)
+					}
+				}
+			}
 		})
 	}
-}
-
-type pair struct{ src, dst string }
-
-type MockUploader struct {
-	Error   error
-	Uploads map[pair]bool
-}
-
-func NewMockUploader() *MockUploader {
-	return &MockUploader{
-		Uploads: make(map[pair]bool),
-	}
-}
-
-func (up *MockUploader) UploadFile(src, dst string, meta *MetaData) error {
-	if up.Error != nil {
-		return up.Error
-	}
-	up.Uploads[pair{src, dst}] = true
-	return nil
 }
