f313bfe89eedce018b432a3e2293f94e7bd7b3a5
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-06-27 16:23:59 -0500

    wrong var was being used for dir structure

--- file_uploader.go
+++ file_uploader.go
@@ -152,7 +152,7 @@ func (up *pelicanFileUploader) UploadFile(src, dst string, meta *MetaData) error
 	defer f.Close()
 
 	//Upload the file to Pelican
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/%s/%s", up.config.Endpoint, up.config.Bucket, src), f)
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/%s/%s", up.config.Endpoint, up.config.Bucket, dst), f)
 	if err != nil {return err}
 	req.Header.Set("Authorization", "Bearer "+string(up.jm.SignedJwtToken))
 	resp, err := up.client.Do(req)
